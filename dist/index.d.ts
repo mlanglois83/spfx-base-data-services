564667abca74c182133da087db548735e2f54a3f
Matthieu LANGLOIS <matthieu.langlois@exakis-nelite.com>
2019-10-08 15:12:00 +0200

    types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,4 @@
 export { initServices } from "./Init/InitServices";
+export * from "./interfaces";
+export * from "./models";
+export * from "./services";
--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,4 @@
 export { initServices } from "./Init/InitServices";
+export * from "./models";
+export * from "./services";
 //# sourceMappingURL=index.js.map
