410b0503f5cb8a190bb6699f9ad98c241de62251
Matthieu LANGLOIS <matthieu.langlois@exakis-nelite.com>
2019-10-09 18:21:22 +0200

    correction config static

--- dist/services/base/BaseService.js
+++ dist/services/base/BaseService.js
@@ -2,9 +2,37 @@ import { sp } from "@pnp/sp";
 import { taxonomy } from "@pnp/sp-taxonomy";
 import { Constants } from "../../constants";
 import { find } from "@microsoft/sp-lodash-subset";
+var _dataServiceConfig = {
+    DbName: "spfx-db",
+    Version: 1,
+    checkOnline: false,
+    context: null,
+    serviceFactory: null,
+    tableNames: [],
+    translations: {
+        AddLabel: "Add",
+        DeleteLabel: "Delete",
+        IndexedDBNotDefined: "IDB not defined",
+        SynchronisationErrorFormat: "Sync error",
+        UpdateLabel: "Update",
+        UploadLabel: "Upload",
+        versionHigherErrorMessage: "Version conflict",
+        typeTranslations: []
+    }
+};
 var BaseService = /** @class */ (function () {
     function BaseService() {
     }
+    Object.defineProperty(BaseService, "Configuration", {
+        get: function () {
+            return _dataServiceConfig;
+        },
+        set: function (configuration) {
+            _dataServiceConfig = configuration;
+        },
+        enumerable: true,
+        configurable: true
+    });
     BaseService.Init = function (configuration) {
         BaseService.Configuration = configuration;
         configuration.tableNames = configuration.tableNames || [];
