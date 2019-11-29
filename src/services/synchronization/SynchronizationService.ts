91551ad733b897cd9329ab17dd5402c5e382aa17
Matthieu LANGLOIS <matthieu.langlois@exakis-nelite.com>
2019-11-29 15:35:40 +0100

    Correction bug synchro

--- src/services/synchronization/SynchronizationService.ts
+++ src/services/synchronization/SynchronizationService.ts
@@ -26,74 +26,70 @@ export class SynchronizationService extends BaseService {
         let errors = [];
         //read transaction table
         let transactions = await this.transactionService.getAll();
-        await Promise.all(transactions.map((transaction, index) => {
-            return new Promise<void>(async(resolve, reject) => {
-                // get associated type & service
-                let itemType = ServicesConfiguration.configuration.serviceFactory.getItemTypeByName(transaction.itemType);
-                let dataService = ServicesConfiguration.configuration.serviceFactory.create(transaction.itemType);
-                // init service for tardive links
-                await dataService.Init();
-                // transform item to destination type
-                let item = assign(new itemType(), transaction.itemData);
-                switch (transaction.title) {
-                    case TransactionType.AddOrUpdate:
-                        const oldId = item.id;
-                        const isAdd = typeof (oldId) === "number" && oldId < 0;       
-                        let dbItem = await dataService.mapItem(item);
-                        const updatedItem = await dataService.addOrUpdateItem(dbItem);
-                        // handle id and version changed
-                        if (isAdd && !updatedItem.error) {
-                            
-                            let nextTransactions: Array<OfflineTransaction> = [];
-                            // next transactions on this item
-                            if (index < transactions.length - 1) {
-                                nextTransactions = await Promise.all(transactions.slice(index + 1).filter((t) => {
-                                    return t.itemType === transaction.itemType &&
-                                        (t.itemData as IBaseItem).id === oldId;
-                                }).map(async (updatedTr) => {
-                                    (updatedTr.itemData as IBaseItem).id = updatedItem.item.id;
-                                    (updatedTr.itemData as IBaseItem).version = updatedItem.item.version;
-                                    let result = await this.transactionService.addOrUpdateItem(updatedTr);
-                                    if(result.error) {
-                                        throw result.error;
-                                    }
-                                    else {
-                                        return result.item;
-                                    }
-                                }));
-                            }
-                            if (dataService.updateLinkedTransactions) {
-                                nextTransactions = await dataService.updateLinkedTransactions(oldId, updatedItem.item.id, nextTransactions);
-                            }
-                            if(index < transactions.length - 1) {
-                                transactions.splice(index + 1, transactions.length - index - 1, ...nextTransactions);
-                            }
+        for (let index = 0; index < transactions.length; index++) {
+            const transaction = transactions[index];
+            // get associated type & service
+            let itemType = ServicesConfiguration.configuration.serviceFactory.getItemTypeByName(transaction.itemType);
+            let dataService = ServicesConfiguration.configuration.serviceFactory.create(transaction.itemType);
+            // init service for tardive links
+            await dataService.Init();
+            // transform item to destination type
+            let item = assign(new itemType(), transaction.itemData);
+            switch (transaction.title) {
+                case TransactionType.AddOrUpdate:
+                    const oldId = item.id;
+                    const isAdd = typeof (oldId) === "number" && oldId < 0;       
+                    let dbItem = await dataService.mapItem(item);
+                    const updatedItem = await dataService.addOrUpdateItem(dbItem);
+                    // handle id and version changed
+                    if (isAdd && !updatedItem.error) {
+                        
+                        let nextTransactions: Array<OfflineTransaction> = [];
+                        // next transactions on this item
+                        if (index < transactions.length - 1) {
+                            nextTransactions = await Promise.all(transactions.slice(index + 1).filter((t) => {
+                                return t.itemType === transaction.itemType &&
+                                    (t.itemData as IBaseItem).id === oldId;
+                            }).map(async (updatedTr) => {
+                                (updatedTr.itemData as IBaseItem).id = updatedItem.item.id;
+                                (updatedTr.itemData as IBaseItem).version = updatedItem.item.version;
+                                let result = await this.transactionService.addOrUpdateItem(updatedTr);
+                                if(result.error) {
+                                    throw result.error;
+                                }
+                                else {
+                                    return result.item;
+                                }
+                            }));
+                        }
+                        if (dataService.updateLinkedTransactions) {
+                            nextTransactions = await dataService.updateLinkedTransactions(oldId, updatedItem.item.id, nextTransactions);
+                        }
+                        if(index < transactions.length - 1) {
+                            transactions.splice(index + 1, transactions.length - index - 1, ...nextTransactions);
+                        }
 
-                        }
-                        if(updatedItem.error) {
-                            errors.push(this.formatError(transaction, updatedItem.error.message));
-                            if(updatedItem.error.name === Constants.Errors.ItemVersionConfict){
-                                await this.transactionService.deleteItem(transaction);
-                            }
-                        }
-                        else {
+                    }
+                    if(updatedItem.error) {
+                        errors.push(this.formatError(transaction, updatedItem.error.message));
+                        if(updatedItem.error.name === Constants.Errors.ItemVersionConfict){
                             await this.transactionService.deleteItem(transaction);
                         }
-                        resolve();
-                        break;
-                    case TransactionType.Delete:
-                        try {
-                            await dataService.deleteItem(item);
-                            await this.transactionService.deleteItem(transaction);
-                            resolve();
-                        } catch (error) {
-                            errors.push(this.formatError(transaction, error.message));
-                            resolve();
-                        }
-                        break;
-                }
-            });            
-        }));
+                    }
+                    else {
+                        await this.transactionService.deleteItem(transaction);
+                    }
+                    break;
+                case TransactionType.Delete:
+                    try {
+                        await dataService.deleteItem(item);
+                        await this.transactionService.deleteItem(transaction);
+                    } catch (error) {
+                        errors.push(this.formatError(transaction, error.message));
+                    }
+                    break;
+            }
+        }        
         //return errors list
         return errors;
     }
